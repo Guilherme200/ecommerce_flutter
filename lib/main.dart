63a1fbc15fc498e5e204d45b2e81e3a46960a76a
Guilherme200 <guimatoso15@gmail.com>
2020-12-11 23:37:34 -0300

    Adição de seeder de produtos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_flutter/providers/providers.dart';
 import 'package:ecommerce_flutter/routes/routes.dart';
+import 'package:ecommerce_flutter/utils/product_seeder.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +8,7 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await ProductSeeder.insertProducts();
   runApp(MyApp());
 }
 
